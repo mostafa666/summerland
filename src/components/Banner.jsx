dcb4a6934577e6171cd80e5908378cf961d8b5c2
erfanwalker <erfankaboli.ce@gmail.com>
2019-10-18 12:31:18 -0700

    Banner body progress 70%

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Banner() {
-  return <div>adfs</div>;
+export default function Banner({ banner }) {
+  console.log(banner);
+  return (
+    <Link
+      to={banner.link}
+      className="banner"
+      style={{ backgroundImage: `url('${banner.srcImg}')` }}
+    />
+  );
 }
--- src/staticData/banner.js
+++ src/staticData/banner.js
@@ -2,11 +2,11 @@ import banner1 from "../common/images/1000009399.jpg";
 import banner2 from "../common/images/05_Ingrid_20191002_ci-xl.jpg";
 
 const indexBanner_1 = {
-  link: "www.example.com",
+  link: "/banner",
   srcImg: banner1
 };
 const indexBanner_2 = {
-  link: "www.example.com",
+  link: "/banner",
   srcImg: banner2
 };
 
