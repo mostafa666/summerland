6fc370b0240179092c5e4ee6ee5ef7989c72f138
erfanwalker <erfankaboli.ce@gmail.com>
2019-10-18 12:03:26 -0700

    IndexInfoIconText body progress 90%

--- src/components/indexComponent/IndexInfoIconText.jsx
+++ src/components/indexComponent/IndexInfoIconText.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function IndexInfoIconText() {
-  return <div>adf</div>;
+  return (
+    <div className="index_info_icon_text">
+      <div className="index_info_icon_text__item">
+        <div />
+        <p>ضمانت اصل بودن کالا</p>
+      </div>
+      <div className="index_info_icon_text__item">
+        <div />
+        <p>ارسال به تمام نقاط کشور</p>
+      </div>
+      <div className="index_info_icon_text__item">
+        <div />
+        <p>قیمت منصفانه</p>
+      </div>
+    </div>
+  );
 }
