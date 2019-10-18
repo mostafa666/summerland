7b494fa7e9d9feb8445c3b79682a618735f1b071
erfanwalker <erfankaboli.ce@gmail.com>
2019-10-18 07:09:42 -0700

    added func for indexPage to app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-import React, { Component } from 'react';
-import UserAccountPage from './components/userAccount/userAccountPage';
-import Signup from './components/signup';
-import UserMenu from './components/userMenu';
-import Cart from './components/cart';
-import FilterBox from './components/filterBox';
-import SliderFade from './components/slider';
-import Signin from './components/signin';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Register from './components/register';
-import Header from './components/Header';
-import DetailPage from './components/detailPage/detalPage';
-import {connect} from 'react-redux';
-import AddComment from './components/detailPage/addCommnent';
-import ProductsPage from './components/pages/ProductsPage';
+import React, { Component } from "react";
+import UserAccountPage from "./components/userAccount/userAccountPage";
+import Signup from "./components/signup";
+import UserMenu from "./components/userMenu";
+import Cart from "./components/cart";
+import FilterBox from "./components/filterBox";
+import SliderFade from "./components/slider";
+import Signin from "./components/signin";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Register from "./components/register";
+import Header from "./components/Header";
+import DetailPage from "./components/detailPage/detalPage";
+import { connect } from "react-redux";
+import AddComment from "./components/detailPage/addCommnent";
+import ProductsPage from "./components/pages/ProductsPage";
+import IndexPage from "./components/pages/IndexPage";
 
 const DemoMenu = () => (
-    <React.Fragment>
-        <Header />
-        <UserMenu isLogedin={true} />
-    </React.Fragment>
-)
-
-
+  <React.Fragment>
+    <Header />
+    <UserMenu isLogedin={true} />
+  </React.Fragment>
+);
 
 class App extends Component {
-    
-    render() {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/account" component={UserAccountPage} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/cart" component={Cart} />
-                    <Route path="/details" component={DetailPage} />
-                    <Route path="/addComment" component={AddComment} />
-                    <Route path="/products" component={ProductsPage} />
-                    <Route path="/" render={() => <UserMenu isLogedin={this.props.state.account.profile.isLogedIn} /> }  />  {/* set islogedin inredux */}
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+
+        <Switch>
+          <Route path="/index" component={IndexPage} />
+          <Route path="/account" component={UserAccountPage} />
+          <Route path="/register" component={Register} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/details" component={DetailPage} />
+          <Route path="/addComment" component={AddComment} />
+          <Route path="/products" component={ProductsPage} />
+          <Route
+            path="/"
+            render={() => (
+              <UserMenu
+                isLogedin={this.props.state.account.profile.isLogedIn}
+              />
+            )}
+          />{" "}
+          {/* set islogedin inredux */}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-export default connect(state => ({state}))(App);
+export default connect(state => ({ state }))(App);
