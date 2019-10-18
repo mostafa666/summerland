76d6efde6ec882ff84b3ecf2b287dec3b8bfab0b
erfanwalker <erfankaboli.ce@gmail.com>
2019-10-18 06:59:10 -0700

    added indexPage fund  body

---
+++ src/components/indexComponent/IndexLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function IndexLayout({ children }) {
+  console.log(children);
+  return (
+    <div>
+      <h1>LayOut Working right</h1>
+    </div>
+  );
+}
